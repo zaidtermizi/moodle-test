066b0e28fafffe8d86c4d2ae3ca792b00a4466bd
zaidtermizi <zaid.termizi@ictinteractive.com>
2017-10-11 14:52:35 +0800

    changed url

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -11572,7 +11572,7 @@ angular.module('mm.core.login', [])
     .state('mm_login.site', {
         url: '/site',
         templateUrl: 'core/components/login/templates/site.html',
-        controller: 'mmLoginSiteCtrl'
+        controller: 'mmLoginSiteCtrl',
 		onEnter:function($state){
 			$state.go('mm_login.credentials',{siteurl:'https://lms.english.com.my'});//direct connection to moodle
 		}
